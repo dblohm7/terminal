a88046a73deda1a8cce06cc17c70c918c58d8573
Aaron Klotz <aklotz@mozilla.com>
2020-10-30 14:55:57 -0600

    Isolate controls

--- dep/NT/ntioapi_x.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#define FILE_SYNCHRONOUS_IO_NONALERT            0x00000020
--- tools/runft.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-rem Run the console feature tests.
-rem Keep this file in sync with tests.xml
-
-call %TAEF% ^
-    %OPENCON%\bin\%ARCH%\Debug\ConHost.Feature.Tests.dll ^
-    %*
